c71d2608eb24d06803a6a2d24bd76c82cbff6f18
Lennart <Lennart@bastl-instruments.com>
2017-11-27 15:15:46 +0100

    dynamic UI

--- src/app.js
+++ src/app.js
@@ -10,104 +10,179 @@ $(function(){
 	});
 	ipcRenderer.send('request_midi_port_options','');
 
-	setInterval(updateProgressBar, 200);
-
-	$("#midifile").on("change", openMIDIFile);
-	$("#flash").on("click", sendStart);
 	$("#logging .toggle").on("click", toggleLog);
 
 	toggleLog();
 
+	changeState(State.INIT);
+
 });
 
+var State = {
+  INIT: 0,
+	LOAD: 1,
+  READY: 2,
+  FLASHING: 3,
+	ABORT: 4,
+	DONE: 5
+};
+var StateNames = ["INIT", "LOAD", "READY", "FLASHING", "ABORT", "DONE"];
+
+var reader;
+
+var currentState = 0;
+
 var midifile = 0;
-var timer = 0;
-var currentMessageID = 0;
-var startTime = 0;
-var endTime = 0;
-var duration = 0;
+
+
+
+
+
+
+
+
+function changeState(state) {
+	currentState = state;
+	console.log("Change State: ", StateNames[currentState]);
+	switch (currentState) {
+		case State.INIT:
+			midifile = 0;
+			break;
+		case State.LOAD:
+			openMIDIFile();
+			break;
+		case State.READY:
+			break;
+		case State.FLASHING:
+			playerStart(midifile);
+			break;
+		case State.ABORT:
+			playerStop();
+			break;
+		case State.DONE:
+			playerStop();
+			break;
+	}
+	setUIFromState();
+}
+
+
+function setUIFromState() {
+	var fileSelector = $("#midifile");
+	var flashButton = $("#flash");
+
+	fileSelector.off();
+	flashButton.off();
+
+	switch (currentState) {
+		case State.INIT:
+			fileSelector.on("change", doLoad);
+			flashButton.addClass("disabled");
+			fileSelector.val("");
+			break;
+		case State.LOAD:
+			setProgress(0);
+			break;
+		case State.READY:
+			flashButton.on("click", doStart);
+			flashButton.text("Flash");
+			flashButton.removeClass("disabled");
+			break;
+		case State.FLASHING:
+			flashButton.on("click", doStop);
+			flashButton.text("Stop");
+			break;
+		case State.ABORT:
+			fileSelector.on("change", doLoad);
+			flashButton.on("click", doStart);
+		case State.DONE:
+			fileSelector.on("change", doLoad);
+			flashButton.text("Flash");
+			printLog("Flashing completed!");
+			setProgress(1000);
+			break;
+	}
+}
+
+function doStart() {
+	changeState(State.FLASHING);
+}
+function doStop() {
+	changeState(State.ABORT);
+}
+function doLoad() {
+	changeState(State.LOAD);
+}
+
+
 
 function openMIDIFile(event) {
-	  var files = event.target.files
+	  var files = $("#midifile").prop("files");
 		if (files) {
 			var thisFile = files[0];
-			var reader = new FileReader();
+			console.log("loading..");
+			reader = new FileReader();
 			reader.onerror = function(error) {
-				showFileOperationStatus("Could not open " + thisFile, true);
-				console.log(err);
+				showFileOperationStatus("Could not open File");
+				//console.log(err);
+				changeState(State.INIT);
 			}
 			reader.onload = function(result) {
 				try {
 					midifile = new MIDIFile(reader.result);
+					console.log("File read");
 					showFileOperationStatus("File loaded", false);
-					parseMIDIfile();
+					printStats(midifile.getEvents());
+					changeState(State.READY);
 				}
 				catch(err) {
 					showFileOperationStatus("Not a valid MIDI File", true);
-					console.log(err);
-					event.target.value = "";
+					//console.log(err);
+					console.log("Invalid file");
+					changeState(State.INIT);
 				}
 			};
 			reader.readAsArrayBuffer(thisFile);
 		}
 }
 
-function parseMIDIfile() {
-	var events = midifile.getEvents();
-	duration = events[events.length-1].playTime;
-	showStats(duration);
-}
 
-function showStats(playTime) {
-	var minutes = Math.floor(playTime / 1000 / 60);
-	var seconds = Math.ceil((playTime / 1000) - minutes*60);
-	var minText = '';
-	if (minutes > 0) {
-		minText = minutes + " minute"
-		if (minutes > 1) minText = minText+'s';
-	}
-	var secText = seconds + " seconds";
-	printLog("Total duration: " + minText +' '+ secText);
-}
+/************ PLAYBACK ***********/
 
-function setMIDIPortOptions(portOptions) {
-	var portSelect = $("#portselect");
-	portSelect.html("");
-	for (var i=0; i<portOptions.length; i++){
-		var el = $("<option></<option>");
-		el.text(portOptions[i]);
-		el.val(i);
-		portSelect.append(el);
-	}
-}
 
-function sendStart() {
-	currentMessageID = 0;
-	timer = 0;
-	startTime = new Date().getTime();
-	endTime = startTime + duration;
-	setProgress(0);
-	sendProceed();
-}
-function sendStop() {
-	timer = 0;
-}
+var currentIndex;
+var events;
+var timer;
+const timeInterval = 10;
+var elapsedTime;
+var endTime;
 
-function sendProceed() {
-	var events = midifile.getEvents();
-	var thisEvent = events[currentMessageID];
-	showMessageDebug(thisEvent);
-	if (currentMessageID < events.length-1) {
-		currentMessageID = currentMessageID + 1;
-		var nextEvent = events[currentMessageID];
-		var timeToNext = nextEvent.playTime - thisEvent.playTime;
-		timer = setTimeout(sendProceed, timeToNext);
-	} else {
-		sendStop();
+function playerStart(midiFile) {
+	console.log("Player: Start");
+	events = midiFile.getEvents();
+	currentIndex = 0;
+	elapsedTime = 0;
+	timer = setInterval(playerCheck, timeInterval);
+	endTime = events[events.length-1].playTime;
+}
+function playerStop() {
+	console.log("Player: Stop");
+	clearTimeout(timer);
+}
+function playerCheck() {
+	elapsedTime = elapsedTime + timeInterval;
+	setProgress(Math.round(elapsedTime/endTime*1000));
+	if (elapsedTime >= events[currentIndex].playTime) {
+		showMessageDebug(events[currentIndex]);
+		currentIndex = currentIndex + 1;
+		if (currentIndex >= events.length-1) {
+			changeState(State.DONE);
+		}
 	}
 }
 
 
+/********** MIDI *****************/
 
 function sendMIDI(data) {
 	console.log("send");
@@ -119,29 +194,37 @@ function sendMIDI(data) {
 
 }
 
+/************ UI **************/
+
+function showMessageDebug(event) {
+	switch (event.type) {
+		case 240:
+			printLog(SysExToString(event.data));
+			break;
+		case 255:
+			// meta event
+			break;
+		default:
+		  printLog("Unexpected event type " + event.type);
+	}
+}
+
+function printStats(events) {
+	var duration = events[events.length-1].playTime;
+	var minutes = Math.floor(duration / 1000 / 60);
+	var seconds = Math.ceil((duration / 1000) - minutes*60);
+	var minText = '';
+	if (minutes > 0) {
+		minText = minutes + " minute"
+		if (minutes != 1) minText = minText+'s';
+	}
+	var secText = seconds + " second";
+	if (seconds != 1) secText = secText+'s';
+	printLog("Total duration:" + minText +' '+ secText);
+}
 
 function showFileOperationStatus(status, error) {
 	printLog(status);
-}
-
-function updateResultField(element, message, isError) {
-	element.text(message);
-	element.fadeIn(100, 'linear');
-	if (isError) {
-		element.addClass("error");
-		element.fadeOut(5000, 'swing');
-	} else {
-		element.removeClass("error");
-		element.fadeOut(3000, 'swing');
-	}
-}
-
-function updateProgressBar() {
-	if (timer) {
-		var now = new Date().getTime();
-		var progress = Math.round((now - startTime)/(duration) * 1000);
-		setProgress(progress);
-	}
 }
 
 function setProgress(val) {
@@ -167,31 +250,33 @@ function toggleLog() {
 	}
 }
 
-function showMessageDebug(event) {
-	switch (event.type) {
-		case 240:
-			printLog(parseSysEx(event.data));
-			break;
-		case 255:
-			// meta event
-			break;
-		default:
-		  printLog("Unexpected event type " + event.type);
-	}
-}
-
 function printLog(text) {
 	var logWindow = $("#logging textarea");
 	logWindow.append(text + "&#10;");
 	logWindow.scrollTop(logWindow.prop('scrollHeight'));
 }
 
+function setMIDIPortOptions(portOptions) {
+	var portSelect = $("#portselect");
+	portSelect.html("");
+	for (var i=0; i<portOptions.length; i++){
+		var el = $("<option></<option>");
+		el.text(portOptions[i]);
+		el.val(i);
+		portSelect.append(el);
+	}
+}
+
+
+
+/********* SysEx ****************/
+
 const Code_Flash = 2;
 const Code_Start = 4;
 const Code_Forward_Flash = 12;
 const Code_Forward_Erase = 15;
 
-function parseSysEx(data) {
+function SysExToString(data) {
 	if (data[0] == 123) {
 
 		var deviceID = data[1];
